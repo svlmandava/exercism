f38af7b70551a2d6a46d46ffa56927faf79a332a
Vijayalakshmi Mandava <svl.mandava@gmail.com>
2021-05-02 19:16:32 +0530

    Made changes as suggested to use data structure(dict) and loop(for).

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,17 +1,6 @@
-def to_rna(dna_strand=''):
-    dna_seq=list('AGTC')
-    rna_seq=list('UCAG')
-    rna=''
+def to_rna(dna_strand):
+    dna_torna={'A':'U','G':'C','T':'A','C':'G'}
+    rna = ''
     for x in dna_strand:
-        if x is dna_seq[0]:
-            rna += rna_seq[0]
-        if x is dna_seq[1]:
-            rna += rna_seq[1]
-        if x is dna_seq[2]:
-            rna += rna_seq[2]
-        if x is dna_seq[3]:
-            rna += rna_seq[3]
-    if rna is not '':
-       return rna
-    else:
-        return ''
+       rna += dna_torna.get(x,rna)
+    return rna
