fd9d6a572c7155db7b17a0ddb74efe16dcd6b372
Vijayalakshmi Mandava <svl.mandava@gmail.com>
2021-05-02 18:48:58 +0530

    Simplified the code as suggested by the mentor.

--- leap/leap.py
+++ leap/leap.py
@@ -1,5 +1,2 @@
 def leap_year(year):
-    if (year%4==0) and ((year%400==0) or (year%100!=0)):
-        return True
-    else:
-        return False
+    return (year % 4 == 0) and ((year % 400 == 0) or (year % 100 != 0))
