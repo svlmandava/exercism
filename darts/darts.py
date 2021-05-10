02c886c33998be17468ec01b14ab606e65647f28
Vijayalakshmi Mandava <svl.mandava@gmail.com>
2021-05-10 22:53:52 +0530

    A function that returns the earned points in a single toss of a Darts game.

--- darts/darts.py
+++ darts/darts.py
@@ -1,2 +1,10 @@
+import math
 def score(x, y):
-    pass
+    dist = math.sqrt(x**2 + y**2)
+    if dist <= 1:
+        return 10
+    if dist <= 5:
+        return 5
+    if dist <= 10:
+        return 1
+    return 0
