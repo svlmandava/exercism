9a9717996826ba24de5d10bfce63c752396c6c80
Vijayalakshmi Mandava <svl.mandava@gmail.com>
2021-05-11 00:06:29 +0530

    A function to return a resistor's band values when the band colors are given.

--- resistor-color-duo/resistor_color_duo.py
+++ resistor-color-duo/resistor_color_duo.py
@@ -1,13 +1,4 @@
-COLORS = {'black' : 0,
-         'brown' : 1,
-         'red' : 2,
-         'orange' : 3,
-         'yellow' : 4,
-         'green' : 5,
-         'blue' : 6,
-         'violet' : 7,
-         'grey' : 8,
-         'white' : 9}
+COLORS = {'black' : 0, 'brown' : 1, 'red' : 2, 'orange' : 3, 'yellow' : 4, 'green' : 5, 'blue' : 6, 'violet' : 7, 'grey' : 8, 'white' : 9}
 
 
 def value(colors):
