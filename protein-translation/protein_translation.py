ea4f9b0c8b929db745ec4188ffc4f2f7a6de0e4f
Vijayalakshmi Mandava <svl.mandava@gmail.com>
2021-05-14 23:23:55 +0530

    A function to translate RNA sequences into proteins.

--- protein-translation/protein_translation.py
+++ protein-translation/protein_translation.py
@@ -1,2 +1,10 @@
+RNASEQ = {"AUG" : "Methionine", "UUU" : "Phenylalanine", "UUC" : "Phenylalanine", "UUA" : "Leucine", "UUG" : "Leucine", "UCU" : "Serine", "UCC" : "Serine", "UCA" : "Serine", "UCG" : "Serine", "UAU" : "Tyrosine", "UAC" : "Tyrosine", "UGU" : "Cysteine", "UGC" : "Cysteine", "UGG" : "Tryptophan", "UAA" : "STOP", "UAG" : "STOP", "UGA" : "STOP"}
+
 def proteins(strand):
-    pass
+    protein = []
+    for x in range(0, len(strand), 3):
+      value = RNASEQ[strand[x : x + 3]]
+      if value == "STOP" :
+        break
+      protein.append(value)
+    return protein
