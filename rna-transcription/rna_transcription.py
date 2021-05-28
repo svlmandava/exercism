a335a58daa2184ca482eb7e7aa52b51a2667e525
Vijayalakshmi Mandava <svl.mandava@gmail.com>
2021-05-28 19:36:48 +0530

    A function to return RNA compliment of a given DNA strand using python generators.

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,4 +1,3 @@
-STRANDS={'A' : 'U', 'G' : 'C', 'T' : 'A', 'C' : 'G'}
+STRANDS = {'A': 'U', 'G': 'C', 'T': 'A', 'C': 'G'}
 def to_rna(dna_strand):
-   rnalist = [STRANDS[x] for x in dna_strand]
-   return ''.join(i for i in rnalist)
+   return ''.join(STRANDS[x] for x in dna_strand)
