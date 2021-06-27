537cacd85ce70137c1a8ae9b181d68da09b27985
Vijayalakshmi Mandava <svl.mandava@gmail.com>
2021-06-27 09:57:53 +0530

    Usage of formatters for wrapping long lines.

--- protein-translation/protein_translation.py
+++ protein-translation/protein_translation.py
@@ -1,10 +1,28 @@
-RNASEQ = {"AUG" : "Methionine", "UUU" : "Phenylalanine", "UUC" : "Phenylalanine", "UUA" : "Leucine", "UUG" : "Leucine", "UCU" : "Serine", "UCC" : "Serine", "UCA" : "Serine", "UCG" : "Serine", "UAU" : "Tyrosine", "UAC" : "Tyrosine", "UGU" : "Cysteine", "UGC" : "Cysteine", "UGG" : "Tryptophan", "UAA" : "STOP", "UAG" : "STOP", "UGA" : "STOP"}
+RNASEQ = {
+    "AUG": "Methionine", 
+    "UUU": "Phenylalanine", 
+    "UUC": "Phenylalanine", 
+    "UUA": "Leucine", 
+    "UUG": "Leucine", 
+    "UCU": "Serine", 
+    "UCC": "Serine", 
+    "UCA": "Serine", 
+    "UCG": "Serine", 
+    "UAU": "Tyrosine", 
+    "UAC": "Tyrosine", 
+    "UGU": "Cysteine", 
+    "UGC": "Cysteine", 
+    "UGG": "Tryptophan", 
+    "UAA": "STOP", 
+    "UAG": "STOP", 
+    "UGA": "STOP"
+    }
 
 def proteins(strand):
     protein = []
     for x in range(0, len(strand), 3):
       value = RNASEQ[strand[x : x + 3]]
-      if value == "STOP" :
+      if value == "STOP":
         break
       protein.append(value)
     return protein
