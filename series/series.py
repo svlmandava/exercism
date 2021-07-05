a7b0bc1e21149d86a73743b394d45cb42f0bca9d
Vijayalakshmi Mandava <svl.mandava@gmail.com>
2021-07-05 12:14:49 +0530

    A program to output all the contiguous substrings of length  in a given string of digits in the order that they appear.

--- series/series.py
+++ series/series.py
@@ -1,4 +1,4 @@
 def slices(series, length):
     if (length > len(series)) or (length <= 0) or (len(series) == ''):
-        raise ValueError('.+')
+        raise ValueError(".+")
     return [''.join(series[i : i + length]) for i in range(len(series) - length + 1)]
