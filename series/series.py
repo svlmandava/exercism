9ce1cfff3182e7fd787e9a64cd97fe687acde51b
Vijayalakshmi Mandava <svl.mandava@gmail.com>
2021-07-05 12:12:02 +0530

    output all the contiguous substrings of length  in

--- series/series.py
+++ series/series.py
@@ -1,2 +1,4 @@
 def slices(series, length):
-    pass
+    if (length > len(series)) or (length <= 0) or (len(series) == ''):
+        raise ValueError('.+')
+    return [''.join(series[i : i + length]) for i in range(len(series) - length + 1)]
