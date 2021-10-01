f6c9660020986133280d847afa393ef6ceddb42b
Vijayalakshmi Mandava <svl.mandava@gmail.com>
2021-10-01 13:28:22 +0530

    Made changes as suggested by the mentor.

--- series/series.py
+++ series/series.py
@@ -1,4 +1,4 @@
 def slices(series, length):
     if length > len(series) or length <= 0:
-        raise ValueError(".+")
+        raise ValueError(".+Slicing is not possible.")
     return [series[i : i + length] for i in range(len(series) - length + 1)]
