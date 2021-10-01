13a82d5fba8c576ccd9984a6b57df06836883f12
Vijayalakshmi Mandava <svl.mandava@gmail.com>
2021-10-01 14:05:07 +0530

    Made changes as suggested by the mentor to reduce the number of lines in code.

--- phone-number/phone_number.py
+++ phone-number/phone_number.py
@@ -3,19 +3,17 @@ class PhoneNumber:
         self.number = number
         self.remove()
         self.valid()
-        self.pretty()
         self.area_code = self.number[:3]
 
     def remove(self):
         self.number = ''.join(x for x in self.number if x.isalnum())
-        self.number = self.number[1:] if self.number[0] == '1' else self.number
+        self.number = self.number.removeprefix('1')
+
 
     def valid(self):
-        if len(self.number) != 10:
-            raise ValueError(".+")
-        if self.number[0] in '01' or self.number[3] in '01':
-            raise ValueError(".+")
+        if len(self.number) != 10 or self.number[0] in '01' or self.number[3] in '01':
+            raise ValueError(".+Please enter the valid phone number.")
     
     def pretty(self):
-        return "({})-{}-{}".format(self.number[:3], self.number[3:6], self.number[6:])
+        return (f"({self.number[:3]})-{self.number[3:6]}-{self.number[6:]}")
 
